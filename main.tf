ca9b209f4409ad1649121d16324d856a538d2f11
dong huang <huangdgm@gmail.com>
2021-03-16 16:45:50 +0800

    Decommission a single web server

--- main.tf
+++ main.tf
@@ -11,40 +11,15 @@ data "aws_subnet_ids" "default" {
 	vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_instance" "example" {
-	ami 				= "ami-07a0844029df33d7d"
-	instance_type 			= "t2.micro"
-	vpc_security_group_ids 		= [aws_security_group.instance.id]
-
-	user_data = <<-EOF
-		    #!/bin/bash
-		    sudo yum update -y
-                    sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-		    sudo yum install -y httpd mariadb-server
-		    sudo systemctl start httpd
-		    sudo systemctl enable httpd
-		    sudo usermod -a -G apache ec2-user
-		    sudo chown -R ec2-user:apache /var/www
-		    sudo chmod -R 777 /var/www
-		    sudo find /var/www -type d -exec chmod 2775 {} \;
-		    sudo find /var/www -type f -exec chmod 0664 {} \;
-		    sudo echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
-		    EOF
-
-	tags = {
-		Name = "terraform-example"
-	}
-}
-
 resource "aws_launch_configuration" "example" {
 	image_id			= "ami-07a0844029df33d7d"
-	instance_type			= "t2.micro"
-	security_groups			= [aws_security_group.instance.id]
+	instance_type		= "t2.micro"
+	security_groups		= [aws_security_group.instance.id]
 
 	user_data = <<-EOF
 		    #!/bin/bash
 		    sudo yum update -y
-                    sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+			sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 		    sudo yum install -y httpd mariadb-server
 		    sudo systemctl start httpd
 		    sudo systemctl enable httpd
